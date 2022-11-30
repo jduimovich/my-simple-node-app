9adc66b0cef464b4a3b51a68adfadcde7ea9fe94
John Duimovich <jduimovich@gmail.com>
2022-11-30 15:41:36 -0500

    http://localhost:8080/

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ function run_web_site(port) {
 	app.use("/", express.Router());   
 	app.listen(port, function () {
 		console.log("Static site hosted on port", port);
+		console.log("http://localhost:8080");
+		
 	});
 } 
 run_web_site(8080) 
